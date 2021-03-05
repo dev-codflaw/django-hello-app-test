97bb2ccea130162ca892da2103f116943bc434ef
Umesh <dev.codflaw@gmail.com>
2021-03-05 14:01:37 +0530

    first commit

---
+++ hello/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+
+class HelloView(APIView):
+    
+    def get(self, request):
+        content = {'message': 'hello world!'}
+        return Response(content)
